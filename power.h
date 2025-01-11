48d9e9185825c8ec3ddb62eed381e2b1c781fa72
BronyNoob <109363868+BronyNoob@users.noreply.github.com>
2025-01-11 12:02:19 +0500

    Update power.h

--- power.h
+++ power.h
@@ -1,6 +1,8 @@
 #ifndef POWER_H
 #define POWER_H
 
-long long power(long long a, int n);
+#include <string>
+std::string multiply_strings(const std::string &num1, const std::string &num2);
+bool is_digits(const std::string &str);
 
 #endif
