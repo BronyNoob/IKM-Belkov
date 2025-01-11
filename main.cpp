fe8edb91e672cb6b0f3a0bfb7a308600f9a3e3e3
BronyNoob <109363868+BronyNoob@users.noreply.github.com>
2025-01-11 12:08:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,33 @@
-#include <iostream>
-#include "power.h"
+   #include <iostream>
+   #include <string>
+   #include "power.h"
 
-int main() {
-    long long a;
-    int n;
+   int main() {
+       std::string a, n_str;
 
-    std::cout << "Введите значение a: ";
-    while (!(std::cin >> a)) {
-        std::cout << "Некорректный ввод! Попробуйте еще раз: ";
-        std::cin.clear();
-        std::cin.ignore(10000, '\n'); // Игнорируем до 10000 символов или до новой строки
-    }
+       std::cout << "Введите значение a: ";
+       while (true) {
+           std::getline(std::cin, a);
+           if (!a.empty() && is_digits(a)) break;
+           std::cout << "Некорректный ввод! Попробуйте еще раз: ";
+       }
 
-    std::cout << "Введите значение n: ";
-    while (!(std::cin >> n) || n < 0) {
-        std::cout << "Некорректный ввод! Степень не может быть отрицательной. Попробуйте еще раз: ";
-        std::cin.clear();
-        std::cin.ignore(10000, '\n'); // Игнорируем до 10000 символов или до новой строки
-    }
+       std::cout << "Введите значение n: ";
+       while (true) {
+           std::getline(std::cin, n_str);
+           if (is_digits(n_str)) {
+               break;
+           } else {
+               std::cout << "Некорректный ввод! Попробуйте еще раз: ";
+           }
+       }
 
-    if (n > 10) {
-        long long result = power(a, n);
-        std::cout << "Результат: " << result << std::endl;
-    } else {
-        std::cout << "Условия задачи не выполнены." << std::endl;
-    }
+       std::string result = "1";
+       for (int i = 0; i < std::stoi(n_str); ++i) {
+           result = multiply_strings(result, a);
+       }
 
-    return 0;
-}
+       std::cout << "Результат: " << result << std::endl;
+
+       return 0;
+   }
